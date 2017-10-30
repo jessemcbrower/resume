c7a3c8fc6368cc08565a869ec53bf95bfcc9a3f3
Jesse <browerpowr@gmail.com>
2017-10-29 21:40:31 -0400

    tweaked responsiveness in anticipation of some updates

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -6,14 +6,14 @@ $(".topnav li a").filter(function(){
     return this.href == location.href.replace(/#.*/, "");
 }).addClass("active");
 
-// Window Resize Behavior
+// // Window Resize Behavior
 
-	$(window).resize(function() {
-	if ($(window).width() < 950) {
-     	$('html').css("font-size", "1.7vw");
-  	} else {
-    	//
-    }
-});
+// 	$(window).resize(function() {
+// 	if ($(window).width() < 950) {
+//      	$('html').css("font-size", "1.7vw");
+//   	} else {
+//     	//
+//     }
+// });
 
 });
